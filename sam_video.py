02872123d72d11f6f7f8c76c2568220f6604f3d6
gabi.shahmayster <shahmayster@gmail.com>
2024-09-16 00:33:11 +0300

    -added saving/loading of processed frames

--- sam_video.py
+++ sam_video.py
@@ -1,4 +1,6 @@
+import dataclasses
 import os
+import pickle
 import time
 
 import cv2
@@ -63,15 +65,21 @@ def get_bounding_box(mask: np.ndarray, pad: int = 0) -> BoundingBox:
     return BoundingBox((max(0, top_left_x-pad), max(0, top_left_y-pad)),
                        (min(mask.shape[1], bottom_right_x+pad), min(mask.shape[0], bottom_right_y+pad)))
 
-class ChalkGpt:
+@dataclasses.dataclass
+class ChalkGptConfig:
+    save_to_disk: bool
+    try_to_load_from_disk: bool
     video_dir: str
+
+class ChalkGpt:
+    config: ChalkGptConfig
     predictor: object
     pose_estimator: object
 
     CLIMBER_OBJECT_ID = 1
 
-    def __init__(self, video_dir: str):
-        self.video_dir = video_dir
+    def __init__(self, config: ChalkGptConfig):
+        self.config = config
 
         sam2_checkpoint = "checkpoints/sam2_hiera_small.pt"
         model_cfg = "sam2_hiera_s.yaml"
@@ -80,17 +88,40 @@ class ChalkGpt:
         self.yolo_nas = super_gradients.training.models.get("yolo_nas_pose_l", pretrained_weights="coco_pose").cuda()
         self.yolo_nas.eval()
 
+    def main(self):
+        process_frames: bool = True
+        os.makedirs('cache', exist_ok=True)
+        output_path: str = os.path.join('cache',self.config.video_dir+'.pkl')
+        if self.config.try_to_load_from_disk and os.path.exists(output_path):
+            with open(output_path, 'rb') as f:
+                saved_data = pickle.load(f)
+                frame_names = saved_data['frame_names']
+                video_segments = saved_data['video_segments']
+                process_frames = False
 
-    def run(self):
+        if process_frames:
+            frame_names = self.init_frames()
+            video_segments = self.process_frames(frame_names=frame_names)
+            save_data: Dict = {"frame_names":frame_names,
+                         "video_segments":video_segments}
+            if self.config.save_to_disk:
+                with open(output_path, 'wb') as f:
+                    pickle.dump(save_data, f)
+
+        self.visualize(video_segments=video_segments, frame_names=frame_names)
+
+    def init_frames(self):
+        frame_names = [
+            p for p in os.listdir(self.config.video_dir)
+            if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG"]
+        ]
+        frame_names.sort(key=lambda p: int(os.path.splitext(p)[0]))
+        return frame_names
+
+    def process_frames(self, frame_names):
         with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
-            inference_state = self.predictor.init_state(video_path=self.video_dir, offload_video_to_cpu=True,
+            inference_state = self.predictor.init_state(video_path=self.config.video_dir, offload_video_to_cpu=True,
                                                    offload_state_to_cpu=True)
-
-            frame_names = [
-                p for p in os.listdir(self.video_dir)
-                if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG"]
-            ]
-            frame_names.sort(key=lambda p: int(os.path.splitext(p)[0]))
 
             ann_frame_idx = 0
             ann_obj_id = self.CLIMBER_OBJECT_ID
@@ -112,32 +143,37 @@ class ChalkGpt:
                     for i, out_obj_id in enumerate(out_obj_ids)
                 }
 
-            for out_frame_idx in range(0, len(frame_names)):
-                for out_obj_id, out_mask in video_segments[out_frame_idx].items():
-                    if out_obj_id != self.CLIMBER_OBJECT_ID:
-                        continue
-                    out_mask = out_mask.squeeze()
-                    bbox = get_bounding_box(mask=out_mask, pad=50)
-                    frame: np.ndarray = cv2.imread(os.path.join(self.video_dir, frame_names[out_frame_idx]))
-                    climber_crop = frame[bbox.top_left[1]:bbox.bottom_right[1], bbox.top_left[0]:bbox.bottom_right[0]]
-                    cv2.imshow("climber crop", climber_crop)
+            return video_segments
 
-                    pose: ImagePoseEstimationPrediction = self.yolo_nas.predict(climber_crop, conf=0.3, fuse_model=False)
-                    pose_draw = pose.draw()
-                    cv2.imshow("climber pose", pose_draw)
+    def visualize(self, video_segments, frame_names):
+        for out_frame_idx in range(0, len(frame_names)):
+            for out_obj_id, out_mask in video_segments[out_frame_idx].items():
+                if out_obj_id != self.CLIMBER_OBJECT_ID:
+                    continue
+                out_mask = out_mask.squeeze()
+                bbox = get_bounding_box(mask=out_mask, pad=50)
+                frame: np.ndarray = cv2.imread(os.path.join(self.config.video_dir, frame_names[out_frame_idx]))
+                climber_crop = frame[bbox.top_left[1]:bbox.bottom_right[1], bbox.top_left[0]:bbox.bottom_right[0]]
+                cv2.imshow("climber crop", climber_crop)
 
-                    mask_3d = np.stack((0 * out_mask, 0 * out_mask, out_mask), axis=2).astype(np.uint8)
-                    blend = cv2.blendLinear(frame, 255 * mask_3d, 0.5 * np.ones_like(out_mask, dtype=np.float32),
-                                            0.5 * np.ones_like(out_mask, dtype=np.float32))
+                pose: ImagePoseEstimationPrediction = self.yolo_nas.predict(climber_crop, conf=0.3, fuse_model=False)
+                pose_draw = pose.draw()
+                cv2.imshow("climber pose", pose_draw)
 
-                    cv2.imshow("blended frame", blend)
+                mask_3d = np.stack((0 * out_mask, 0 * out_mask, out_mask), axis=2).astype(np.uint8)
+                blend = cv2.blendLinear(frame, 255 * mask_3d, 0.5 * np.ones_like(out_mask, dtype=np.float32),
+                                        0.5 * np.ones_like(out_mask, dtype=np.float32))
 
-                    while True:
-                        key = cv2.waitKey(1) & 0xFF
-                        if key == 32:  # 32 is the ASCII code for spacebar
-                            break
+                cv2.imshow("blended frame", blend)
+
+                while True:
+                    key = cv2.waitKey(1) & 0xFF
+                    if key == 32:  # 32 is the ASCII code for spacebar
+                        break
 
 if __name__ == "__main__":
-    chalk_gpt: ChalkGpt = ChalkGpt(video_dir='downloaded_frames_tag')
-    chalk_gpt.run()
-
+    config: ChalkGptConfig = ChalkGptConfig(save_to_disk=True,
+                                            try_to_load_from_disk=True,
+                                            video_dir='downloaded_frames_tag')
+    chalk_gpt: ChalkGpt = ChalkGpt(config)
+    chalk_gpt.main()
